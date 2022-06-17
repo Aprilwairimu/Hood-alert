90e65a85ffb88438ae6fba79d9a7c537b9f779d1
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-06-17 12:58:38 +0300

    created login and registration function

---
+++ hood/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import models
+from django import forms
+
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username","email","password1","password2"]
+
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=50)
+    password=forms.CharField(max_length=20, widget=forms.PasswordInput)
--- hood/views.py
+++ hood/views.py
@@ -6,3 +6,35 @@ from django.http  import HttpResponse,Http404
 def home(request):
 
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request,'register/register.html', {"form":form})
+
+def login_user(request):
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                return HttpResponse('Such a user does not exist')
+        else:
+            return HttpResponse("Form is not Valid")
+    return render(request,'register/login.html',{'form':form})
+
+def logout(request):
+    logout(request)
+    return redirect('home')
