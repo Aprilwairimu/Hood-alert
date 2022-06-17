e45c0ef5552d976db6f24b08bd4e2865dde9b8f4
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-06-17 20:08:11 +0300

    added an image icon

--- hood/urls.py
+++ hood/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 
 urlpatterns =[
-    path('landing/',views.landing, name='landing'),
     path('',views.home, name='home'),
     path('register',views.register, name='register',),
     path('login',views.login_user,name='login'),
