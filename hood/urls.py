3e61707c74bb401a0ba5bb456bb145a6c1bcbc49
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-06-17 19:37:35 +0300

    added an image background

--- hood/urls.py
+++ hood/urls.py
@@ -5,13 +5,13 @@ from django.conf import settings
 
 
 urlpatterns =[
-    path('landing',views.landing, name='landing'),
-    path('home',views.home, name='home'),
+    path('',views.landing, name='landing'),
+    path('',views.home, name='home'),
     path('register',views.register, name='register',),
-    path('login/',views.login_user,name='login'),
-    path('logout/',views.logout,name='logout'),
-    path('create_profile/<user_id>', views.create_profile, name="create_profile"),
-    path('profile/<str:pk>', views.profile, name="profile"),
+    path('login',views.login_user,name='login'),
+    path('logout',views.logout,name='logout'),
+    # path('create_profile/<user_id>', views.create_profile, name="create_profile"),
+    # path('profile/<str:pk>', views.profile, name="profile"),
 
-    
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+]
