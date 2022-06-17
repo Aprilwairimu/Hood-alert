c8329df3751b5b97a75602c017dc9164c981a15a
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-06-17 18:18:49 +0300

    created Busimess and post model classes

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
 from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns =[
@@ -11,4 +12,4 @@ urlpatterns =[
     path('logout/',views.logout,name='logout'),
     path('create_profile/<user_id>', views.create_profile, name="create_profile"),
     path('profile/<str:pk>', views.profile, name="profile"),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
